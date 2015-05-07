6391f47586e108a5d5f9f4af685bce57f5e75643
gybin02 <gybin02@163.com>
2015-05-07 15:15:35 +0800

    8. 设置首页跳转。

--- cloud/app.js
+++ cloud/app.js
@@ -12,5 +12,10 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+app.get('/',function(req,res){
+  res.render();
+  res.redirect('/login.html');
+});
+
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
